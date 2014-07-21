42ad78d4dce6ef4310c61dc13a314dffd5ecf1ad
Logan Garbarini <logangarbarini@gmail.com>
2014-07-20 22:06:55 -0600

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuftsorder::Application.config.secret_key_base = '00113315d5d9544c4fb4249d2ddd265db2bd7de286a1ef8f6ba55750bdbbc0efecbfbb6debc900e2b0b6d5fa65a26423b346d9fd4094141c88cd700ce724af02'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tuftsorder::Application.config.session_store :cookie_store, key: '_tuftsorder_session'
